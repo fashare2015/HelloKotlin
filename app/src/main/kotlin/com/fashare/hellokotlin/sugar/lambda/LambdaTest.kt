d7e4659a4e673bae7aa70bec936073e063a8a24f
apple <153614131@qq.com>
2017-06-01 03:28:39 +0800

    update

--- app/src/main/kotlin/com/fashare/hellokotlin/sugar/lambda/LambdaTest.kt
+++ app/src/main/kotlin/com/fashare/hellokotlin/sugar/lambda/LambdaTest.kt
@@ -7,8 +7,26 @@ package com.fashare.hellokotlin.sugar.lambda
  */
 fun main(args: Array<String>) {
     // new 一个线程
-    val runnable = { println("I'm a Lambda") }
-    Thread(runnable).start()
+    // 匿名类写法
+    val runnable1 = object : Runnable{
+        override fun run() {
+            println("I'm an anonymous class")
+        }
+    }
+
+    // 函数写法, 略像js
+    val runnable2 = fun (){
+        println("I'm a function")
+    }
+
+    // lambda写法1
+    val runnable3 = Runnable { ->
+        println("I'm a Lambda")
+    }
+
+    // lambda写法2
+    val runnable4 = { println("I'm a Lambda") }
+    Thread(runnable4).start()
 
     // 自定义高阶函数, lambda 表达式 作为入参
     listOf("1", "2", "3", "4").myForEach { println(it) }
